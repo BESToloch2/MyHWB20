f1f09cdfde81ccccd55822b9f2c2f2467b6a6d75
BESToloch2 <befanalex2@gmail.com>
2024-08-17 02:34:12 -0500

    2nd commit

---
+++ src/AsgharHW/E1Credentials.java
@@ -0,0 +1,7 @@
+package AsgharHW;
+
+public class E1Credentials {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/AsgharHW/E2ChildName.java
@@ -0,0 +1,7 @@
+package AsgharHW;
+
+public class E2ChildName {
+    public static void main(String[] args) {
+
+    }
+}
