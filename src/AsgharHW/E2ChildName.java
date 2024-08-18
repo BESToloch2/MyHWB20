1c02d00fe3a68d19b2e4df26200fa6c59f0e1629
BESToloch2 <befanalex2@gmail.com>
2024-08-18 00:50:21 -0500

    finished 2nd ex "Name for the baby"

--- src/AsgharHW/E2ChildName.java
+++ src/AsgharHW/E2ChildName.java
@@ -1,10 +1,12 @@
 package AsgharHW;
 
+import java.util.Scanner;
+
 public class E2ChildName {
     public static void main(String[] args) {
 /*
 Write a program that reads two people's first
-names and if they expecting boy or girl?
+names and if they are expecting boy or girl?
 Based on the input suggests a name for a baby:
 Example Output:
 Mom’s first name? Mary
@@ -18,5 +20,21 @@ Dad’s first name? Daniel
 Boy or Girl? girl
 Suggested baby name: MAIEL
  */
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter mother first name:");
+        String matherName=input.nextLine();
+        System.out.println("Enter the expected gander(Boy/Girl) for baby:");
+        String babyGenderM=input.nextLine();
+
+        System.out.println("Enter father first name:");
+        String fatherName=input.nextLine();
+        System.out.println("(Father)Enter the expected gender(Boy/Girl) for baby:");
+        String babyGenderF=input.nextLine();
+
+         if (babyGenderM.equalsIgnoreCase("Boy")&& babyGenderF.equalsIgnoreCase("Boy")){
+             System.out.println("Suggested baby name: "+fatherName);
+         }else if (babyGenderM.equalsIgnoreCase("Girl")&&babyGenderF.equalsIgnoreCase("Girl")){
+             System.out.println("Suggested baby name: "+matherName);
+         }
     }
 }
